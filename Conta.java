b7b79ee7748261f3eae1539e8eeb919eea9b236f
Arthur Rabelo <rabeloarthur@ufpi.edu.br>
2026-09-22 10:15:04 -0300

    Criar classe Conta com atributos numero e saldo #18

---
+++ Conta.java
@@ -0,0 +1,27 @@
+public class Conta {
+    private int numero;
+    private double saldo;
+
+    public Conta(int numero) {
+        this.numero = numero;
+        this.saldo = 0.0;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            System.out.println("Depósito de R$ " + valor + " realizado com sucesso!");
+            System.out.println("Novo saldo: R$ " + this.saldo);
+        } else {
+            System.out.println("Erro: O valor do depósito deve ser maior que zero!");
+        }
+    }
+}
