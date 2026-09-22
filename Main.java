70fe4adf06c5801cc2f0ef5a07085ebe2940b546
Arthur Rabelo <rabeloarthur@ufpi.edu.br>
2026-09-22 10:16:06 -0300

    Adicionar funcionalidade de depósito no menu de operações e lista de contas #18

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 
         ArrayList clientes = new ArrayList<Cliente>();
+        ArrayList<Conta> contas = new ArrayList<Conta>();
 
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
 		int op = mainMenu.getSelection();
@@ -16,6 +17,46 @@ public class Main {
         while (op!=4){
             System.out.println( op + "foi selecionada");
             switch (op) {
+                case 3:
+                    Menu menuOperacoes = new Menu("Menu Operacoes", Arrays.asList("Depositar", "Voltar"));
+                    int op3 = menuOperacoes.getSelection();
+                    System.out.println(op3 + " foi selecionada");
+                    while (op3 != 2) {
+                        if (op3 == 1) {
+                            System.out.println("Informe o número da conta:");
+                            Scanner scanConta = new Scanner(System.in);
+                            int numeroConta = 0;
+                            try {
+                                numeroConta = Integer.parseInt(scanConta.nextLine());
+                            } catch (NumberFormatException e) {
+                                System.out.println("Número inválido!");
+                            }
+
+                            Conta contaEncontrada = null;
+                            for (Conta c : contas) {
+                                if (c.getNumero() == numeroConta) {
+                                    contaEncontrada = c;
+                                    break;
+                                }
+                            }
+
+                            if (contaEncontrada != null) {
+                                System.out.println("Informe o valor do depósito:");
+                                Scanner scanValor = new Scanner(System.in);
+                                double valor = 0;
+                                try {
+                                    valor = Double.parseDouble(scanValor.nextLine());
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Valor inválido!");
+                                }
+                                contaEncontrada.depositar(valor);
+                            } else {
+                                System.out.println("Conta não encontrada!");
+                            }
+                        }
+                        op3 = menuOperacoes.getSelection();
+                    }
+                    break;
                 case 2:
                     Menu menuCliente = new Menu("Menu Cliente", Arrays.aslist("Cadastrar Cliente", "Encontrar Cliente", "Voltar"));
                     int op1 = menuCliente.getSelection();
